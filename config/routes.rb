24c92a3b81ce18685040b0d5c2c83d4a18674248
cavani <afcavani@gmail.com>
2011-04-14 15:54:19 -0300

    removed default rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 First::Application.routes.draw do
+
+match '/contact', :to => 'pages#contact'
+match '/about',   :to => 'pages#about'
+match '/help',    :to => 'pages#help'
+
   get "pages/home"
 
   get "pages/contact"
 
   get "pages/about"
-
+  root :to => 'pages#home'
   resources :microposts
 
   resources :users
